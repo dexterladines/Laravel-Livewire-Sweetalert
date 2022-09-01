2a56f06c697483e3504498273163929ea536be33
Dexter Ladines <ladines.dexter@nexseed.net>
2022-09-01 09:50:31 +0800

    done with laravel-livewire-sweetlertnotification

---
+++ resources/views/livewire/notification-sweet-alert.blade.php
@@ -0,0 +1,6 @@
+<div class="text-center m-5">
+    <h1 >Laravel Livewire Sweetalert Example</h1>
+   
+    <button type="button" wire:click="alertSuccess" class="btn btn-success mt-3">Success Alert</button>
+    <button type="button" wire:click="alertConfirm" class="btn btn-danger mt-3">Confirm Box</button>
+</div>
